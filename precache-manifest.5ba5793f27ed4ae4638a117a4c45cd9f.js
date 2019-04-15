cac4cd6371fff109fab367450671331df37ad507
Jiansen <jiansenhe@gmail.com>
2019-04-16 00:56:57 +0100

    build

---
+++ precache-manifest.5ba5793f27ed4ae4638a117a4c45cd9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b8f8daf984c503fdd25",
+    "url": "/InvestechAITagDemo/static/css/main.11d04842.chunk.css"
+  },
+  {
+    "revision": "5b8f8daf984c503fdd25",
+    "url": "/InvestechAITagDemo/static/js/main.ab416b7b.chunk.js"
+  },
+  {
+    "revision": "568a01c0740551c20a08",
+    "url": "/InvestechAITagDemo/static/js/runtime~main.cec3c655.js"
+  },
+  {
+    "revision": "fd47f16656155b8af13f",
+    "url": "/InvestechAITagDemo/static/js/2.aee74031.chunk.js"
+  },
+  {
+    "revision": "78128caf946adc22950c9c8b036de817",
+    "url": "/InvestechAITagDemo/index.html"
+  }
+];
